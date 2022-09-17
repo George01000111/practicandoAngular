a9f0c769fa4b54aa2562ac7ac312895c47c25361
george <george_soft_01@hotmail.com>
2022-09-17 18:40:57 -0500

    subiendo por primera vez

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '',
+    //component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full',
+      },
+      {
+        path: 'home',
+        loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+      },
+    ]
+  },
+
+  {
+    path: 'admin',
+
+    loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
