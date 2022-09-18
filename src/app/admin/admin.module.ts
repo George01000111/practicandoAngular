7d58885ae85354825a7cc387d597e78689e00d0a
george <george_soft_01@hotmail.com>
2022-09-17 19:35:18 -0500

    instertandi

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { NavComponent } from './components/nav/nav.component';
+import { MaterialModule } from './../material/material.module';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { NavComponent } from './components/nav/nav.component';
   ],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    MaterialModule
   ]
 })
 export class AdminModule { }
